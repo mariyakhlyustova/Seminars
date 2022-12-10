73f5cfc3d6f132ff484aa1a52290113d82ec0116
Mariya Khlyustova <7047373@mail.ru>
2022-12-10 16:58:46 +0300

    dz4

--- DZ/DZ4/Z25/Program.cs
+++ DZ/DZ4/Z25/Program.cs
@@ -2,22 +2,27 @@
 // и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+// Ограничения:
+// - Нельзя использовать класс Math
+// - Должна быть отдельная функция Power, которая возвразает результат
+// - Внутри функций НЕЛЬЗЯ использовать класс Console. Только в основном блоке кода
 
 Console.WriteLine("A= ");
 int numA = int.Parse(Console.ReadLine()!);
 Console.WriteLine("B= ");
 int numB = int.Parse(Console.ReadLine()!);
 
-// 1 способ через функцию Math.Pow(), по рекомендации на семинаре.
 
-Console.WriteLine(Math.Pow(numA, numB));
+int expFunction = Power(numA, numB);
+Console.WriteLine(expFunction);
 
 
-// 2 способ через цикл как указано в условии задачи.
-
-// int expAinB = numA;
-// for (int i = 1; i < numB; i++)
-// {
-//     expAinB = expAinB * numA;
-// }
-// Console.WriteLine(expAinB);
+int Power(int numberA, int numberB)
+{
+    int expAinB = numberA;
+    for (int i = 1; i < numberB; i++)
+    {
+        expAinB = expAinB * numberA;
+    }
+    return expAinB;
+}
--- DZ/DZ4/Z29/Program.cs
+++ DZ/DZ4/Z29/Program.cs
@@ -1,8 +1,15 @@
-﻿// Задача 29: Напишите программу, которая принимает число N и  задаёт массив
-// из N элементов и выводит их на экран.
+﻿// Напишите программу, которая принимает на вход числа n, min, max.
+// Затем генерирует массив размером n и заполняет его случайными 
+// числами в диапазоне от min до max.
+// 5, 1, 19 -> [1, 2, 5, 7, 19]
+// 3, 1, 33 -> [6, 1, 33]
 
-Console.WriteLine("Введите число");
+Console.WriteLine("Введите число n");
 int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число min");
+int min = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число max ");
+int max = int.Parse(Console.ReadLine()!);
 
 int[] array = new int[num];
 
@@ -14,7 +21,7 @@ void FillArray(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        array[i] = new Random().Next(0, num);
+        array[i] = new Random().Next(min, max + 1);
     }
 }
 
