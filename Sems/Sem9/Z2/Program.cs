22f6813e850146a4262f141348c32c52b27a95b7
Mariya Khlyustova <7047373@mail.ru>
2022-12-25 22:25:47 +0300

    sem 9

---
+++ Sems/Sem9/Z2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+Console.WriteLine("Введите m");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите n");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(FromMToN(m, n));
+
+string FromMToN(int m, int n)
+{
+   
+    if (m == n)
+    {
+        return m.ToString();
+    }
+    else
+    {
+        return m.ToString() + ", " + FromMToN(m + 1, n);
+    }
+}
---
+++ Sems/Sem9/Z3/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 67: Напишите программу, которая будет принимать 
+// на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+// Задача 69: Напишите программу, которая на вход принимает 
+// два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+// 67
+Console.WriteLine("Введите a");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите b");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(MyPower(m, n));
+
+//69
+Console.WriteLine("Введите n");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine(SumOfNumbers(n));
+
+int SumOfNumbers(int n)
+{
+    if (n / 10 == 0)
+    {
+        return n;
+    }
+    else
+    {
+        return n % 10 + SumOfNumbers(n / 10);
+    }
+}
+
+int MyPower(int a, int b)
+{
+    if (b == 1)
+    {
+        return a;
+    }
+    else
+    {
+        return a * MyPower(a, b - 1);
+    }
+}
