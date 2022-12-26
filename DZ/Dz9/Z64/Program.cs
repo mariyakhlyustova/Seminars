5cf2e9354699490fb82889c9fd53b985e1a521b6
Mariya Khlyustova <7047373@mail.ru>
2022-12-26 21:19:49 +0300

    d/z9

---
+++ DZ/Dz9/Z64/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все 
+// натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите N");
+int n = int.Parse(Console.ReadLine()!);
+int m = 1;
+Console.WriteLine(FromN1(m, n));
+
+string FromN1(int m, int n)
+{
+   
+    if (m == n)
+    {
+        return m.ToString();
+    }
+    else
+    {
+        return  FromN1(m + 1, n) + ", " + m.ToString();
+    }
+}
---
+++ DZ/Dz9/Z66/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите M");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите N");
+int n = int.Parse(Console.ReadLine()!);
+
+if (m < n) Console.WriteLine(SumElementsMinM(m, n));
+else Console.WriteLine(SumElementsMaxM(n, m));
+
+int SumElementsMinM(int m, int n)
+{
+    if (m == n)
+    {
+        return n;
+    }
+    else
+    {
+        return n  + SumElementsMinM(m, n - 1);
+    }
+}
+
+int SumElementsMaxM(int n, int m)
+{
+    if (m == n)
+    {
+        return m;
+    }
+    else
+    {
+        return m  + SumElementsMaxM(n, m - 1);
+    }
+}
